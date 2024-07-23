76efe0c374b8ac5aa3f0a6c8ab6065bfdf32577d
Biadetis <j.j.p@mail.de>
2024-07-23 14:01:28 +0200

    Ui Potenz erstellt

--- calc_ui/CalcMainWindow.cpp
+++ calc_ui/CalcMainWindow.cpp
@@ -1,25 +1,36 @@
 #include "CalcMainWindow.h"
 #include "..\calc\framework.h"
 
-CalcMainWindow::CalcMainWindow(QWidget *parent)
-    : QMainWindow(parent)
+CalcMainWindow::CalcMainWindow(QWidget* parent)
+	: QMainWindow(parent)
 {
-    ui.setupUi(this);
-    connect(ui.PotenzExec, &QPushButton::clicked, this, &CalcMainWindow::OnClickPotenzExec);
+	ui.setupUi(this);
+	connect(ui.PotenzExec, &QPushButton::clicked, this, &CalcMainWindow::OnClickPotenzExec);
+
 }
 
 CalcMainWindow::~CalcMainWindow()
 {}
 void CalcMainWindow::OnClickPotenzExec()
 {
-    int u;
-    int i;
-    int result;
-    QString UString = ui.PotenzInput1->toPlainText();
-    QString IString = ui.PotenzInput2->toPlainText();
-    u = UString.toInt();
-    i = IString.toInt();
-    result = PotenzBerechnen(u, i);
-    QString ResultString = QString::number(result);
-    ui.PotenzOutput->setPlainText(ResultString);
+	int u;
+	int i;
+	int result;
+	bool uOK = false;
+	bool iOK = false;
+
+	QString UString = ui.PotenzInput1->text();
+	QString IString = ui.PotenzInput2->text();
+	u = UString.toInt(&uOK);
+	i = IString.toInt(&iOK);
+	if (uOK == false || iOK == false)
+	{
+		ui.PotenzOutput->setHtml("<p style=\"color:red;\">Keine Zahl erkannt!</p>");
+	}
+	else
+	{
+		result = PotenzBerechnen(u, i);
+		QString ResultString = QString::number(result);
+		ui.PotenzOutput->setPlainText(ResultString);
+	}
 }
